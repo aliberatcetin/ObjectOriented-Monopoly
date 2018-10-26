3b418b9d52fd5a03e64bed887a2545e49627c304
Rıdvan Gülcü <ridvangulcu@marun.edu.tr>
2018-10-26 14:21:13 +0300

    Player Class

--- src/main.java
+++ src/main.java
@@ -2,6 +2,6 @@ public class main {
     public static void main(String[] args) {
         System.out.println("Hello world");
 
-        System.out.println("intellij uptade");
+        System.out.println("intellij uptadeş");
     }
 }
