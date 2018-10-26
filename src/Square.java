954eecf7d3a4acb05e03ceed8d54b178e677ec79
beratcetin <aliberatcetinn@gmail.com>
2018-10-26 14:41:33 +0300

    property class

---
+++ src/Square.java
@@ -0,0 +1,18 @@
+
+public class Square {
+	private int Position;
+	private String Name;
+	
+	public int getPosition() {
+		return Position;
+	}
+	public void setPosition(int position) {
+		Position = position;
+	}
+	public String getName() {
+		return Name;
+	}
+	public void setName(String name) {
+		Name = name;
+	}	
+}
