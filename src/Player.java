b2da2e56059d1935cef2aec3d606387e18390690
beratcetin <aliberatcetinn@gmail.com>
2018-10-26 14:44:08 +0300

    neylan

---
+++ src/Player.java
@@ -0,0 +1,74 @@
+import java.util.ArrayList;
+
+
+public class Player {
+    private String name; //Player's name
+    private int balance = 50000; //balance default value
+    private int currentPosition=0; //players current position on the map
+    private boolean isPrison=false; //keeps the player's jail sutiation
+    private ArrayList<Property> propertys=new ArrayList<Property>(); //Keeps lands owned by players
+
+
+    public Player(String name){
+        this.name=name;
+    }
+
+    // increase players balance
+    public void addBalance(int amount){
+        balance+=amount;
+    }
+
+    // decrease players balance
+    public void reduceBalance(int amount){
+        balance-=amount;
+    }
+    // add the new land
+   public void addLand(Property newLand){
+       propertys.add(newLand);
+   }
+    //remove owned land
+   public void removeLand(Property land){
+       propertys.remove(land);
+   }
+
+   //move the player next position
+   public void move(int diceNumber){
+        int prevPosition=currentPosition; //keep previous position
+        currentPosition=(currentPosition+diceNumber)%40;   //add the dice curren position
+        if(currentPosition<prevPosition) addBalance(2000); //if player pass the start point earn tour money
+        if(currentPosition==30){ //if player move to jail zone
+            setPrison(true);
+            currentPosition=10;
+        }
+
+
+   }
+    //teleport user when take luck cards
+   public void teleport(int position){
+        currentPosition=position;
+   }
+
+
+
+    //getter and setter methods
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public ArrayList<Property> getLandCards() {
+        return propertys;
+    }
+
+    public boolean isPrison() {
+        return isPrison;
+    }
+
+    public void setPrison(boolean prison) {
+        isPrison = prison;
+    }
+}
