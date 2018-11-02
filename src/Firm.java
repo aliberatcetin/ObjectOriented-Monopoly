39805cb81facb9386f00bba1d007b535ae837af1
beratcetin <aliberatcetinn@gmail.com>
2018-11-02 18:50:12 +0300

    bug fix about rent arrangement

--- src/Firm.java
+++ src/Firm.java
@@ -1,14 +1,12 @@
 import java.util.Random;
 public class Firm extends Purchasable{
 	
-	private int heyo;
 	public Firm(int position,String name,String color,int rentprice,int purchaseprice,int hypothecprice,int tn) {
 		super(position,name,color,rentprice,purchaseprice,hypothecprice,tn);
 	}
 	
 	
 	public int getRentPrice() {
-		Random random = new Random();
 		int dice1 = (int) (Math.random() * 6 + 1);
 		int dice2 = (int) (Math.random() * 6 + 1);
 		int diceSum = dice1 + dice2;
