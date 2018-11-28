886acc3580c32dd673cc86e15303370e295e1fc8
Rıdvan Gülcü <ridvangulcu@marun.edu.tr>
2018-11-28 03:39:58 +0300

    Fixed inğıt errors.Arrange outputs and messages. Iteration 2

--- src/Firm.java
+++ src/Firm.java
@@ -4,13 +4,15 @@ public class Firm extends Purchasable{
 	public Firm(int position,String name,String color,int rentprice,int purchaseprice,int hypothecprice,int tn) {
 		super(position,name,color,rentprice,purchaseprice,hypothecprice,tn);
 	}
-	
-	//getter
-	public int getRentPrice() {
-		int dice1 = (int) (Math.random() * 6 + 1);
-		int dice2 = (int) (Math.random() * 6 + 1);
-		int diceSum = dice1 + dice2;
-		return diceSum * super.getRentPrice();
+
+	@Override
+	public void Speak(Player player2) {
+		if ( super.isSold() && !player2.getName().equals(getOwner().getName())) {
+			System.out.print(player2.getName()+" moved to " + getName() + "("+ getPosition()+"). " );
+			System.out.println(player2.getName()+" will pay rent("+ getRentPrice() +"$) x Total Dice"+" to "  + getOwner().getName()+". ");
+		}else {
+			System.out.print(player2.getName()+" moved to " + getName()+"("+getPosition()+"). " );
+		}
 	}
 	//getdefaul rent price method
 	//todo
