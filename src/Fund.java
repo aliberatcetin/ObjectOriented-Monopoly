703256e5e9c9b39884e088e2b79135bc3649ad7f
Unknown <aliberatcetinn@gmail.com>
2018-12-07 03:43:24 +0300

    fund cards

--- src/Fund.java
+++ src/Fund.java
@@ -1,10 +1,19 @@
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
 
 //represents fund.
 public class Fund extends UnSalable{
+	private List<Card> Cards = new ArrayList<Card>();
 	public Fund(int position,String name,String color) {
 		super(position,name,color);
+		for(int i=0;i<6;i++) {
+			Cards.add(new Card(i+1));
+		}
 	}
 	
+	
+
 	@Override
 	public void Speak(Player player) {
 		System.out.print(player.getName() +" comes "+getName()+"("+getPosition() +"). ");
@@ -12,9 +21,25 @@ public class Fund extends UnSalable{
 
 	@Override
 	public void event(Player player) {
+		int randomCard=(int) (Math.random() * 6 );
+		Card c = Cards.get(randomCard);
+		try {
+			transaction(c,player);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 		
-		// TODO Auto-generated method stub
-		
+	}
+	public void transaction(Card card,Player player)throws Exception{
+        
+        // Creating class object from the object using 
+        // getclass method 
+        Class cls = card.getClass(); 
+        String methodName = card.methodType();
+        Method methodcall1 = cls.getDeclaredMethod(methodName,Player.class); 
+        
+        methodcall1.invoke(card,player);
 	}
 	
 	
